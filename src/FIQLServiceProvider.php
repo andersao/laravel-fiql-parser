6b8be471897ba386bb0cfa6d8856435f9844b716
Anderson Andrade <anderson.asa89@gmail.com>
2021-10-14 08:40:13 -0300

    add tests

--- src/FIQLServiceProvider.php
+++ src/FIQLServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
+
 namespace Prettus\Laravel\FIQL;
 
 use Illuminate\Support\ServiceProvider;
 
-class FIQLServiceProvider extends ServiceProvider {
-    
+class FIQLServiceProvider extends ServiceProvider
+{
 }
